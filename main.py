c91da48a7dcce8a808197a98c3506a3ce2bdd7c3
M Wong <wongmt15@gmail.com>
2024-02-24 04:24:52 +1100

    24-2

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ def cv():
     return render_template('cv.html')
 @app.route("/photos")
 def photos():
-    return render_template('photos.html')
+    return render_template('photos.html')   
 @app.route("/blog")
 def blog():
     DATABASE = 'db.sqlite'
     con = sqlite3.connect(DATABASE)
     con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("select * from tb1 order by id desc")
-    rows = cur.fetchall()  
+    rows = con.execute("select * from tb1 order by id desc").fetchall()
+    con.close()
     return render_template("blog.html", rows=rows)    
     
 if __name__=="__main__":
